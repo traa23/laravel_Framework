3e817848c9c855bcf29798f027949fa139039129
Anugrah Putra Fajar <putrafajar2366@gmail.com>
2025-10-28 19:21:57 +0700

    28/10/2025

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
     public function index()
     {
         $data['dataUser'] = User::all();
-		return view('admin.user.index',$data);
+        return view('admin.user.index', $data);
     }
 
     /**
@@ -32,12 +32,12 @@ class UserController extends Controller
     public function store(Request $request)
     {
         $data['name'] = $request->name;
-		$data['email'] = $request->email;
-		$data['password'] = $data['password'] = Hash::make($request->password);
+        $data['email'] = $request->email;
+        $data['password'] = $data['password'] = Hash::make($request->password);
 
-		User::create($data);
+        User::create($data);
 
-		return redirect()->route('user.index')->with('success','Penambahan Data Berhasil!');
+        return redirect()->route('user.index')->with('success', 'Penambahan Data Berhasil!');
     }
 
     /**
@@ -53,8 +53,8 @@ class UserController extends Controller
      */
     public function edit(string $id)
     {
-         $data['dataUser'] = User::findOrFail($id);
-    return view('admin.user.edit', $data);
+        $data['dataUser'] = User::findOrFail($id);
+        return view('admin.user.edit', $data);
     }
 
     /**
@@ -62,12 +62,21 @@ class UserController extends Controller
      */
     public function update(Request $request, string $id)
     {
-         $id = $id;
+        $id = $id;
         $user = User::findOrFail($id);
 
         $user->name = $request->name;
         $user->email = $request->email;
-        $user->password = $request->password;
+        //ini tidak boleh kosong atau null
+        //$user->password = $request->password;
+
+        //versi tidak eror
+        if ($request->filled('password')) {
+            $user->password = bcrypt($request->password); // gunakan bcrypt untuk hashing
+        }
+
+        $user->save();
+        return redirect()->route('user.index')->with('success', 'Data Berhasil Diupdate!');
 
         $user->save();
         return redirect()->route('user.index')->with('success', 'Data Berhasil Diupdate!');
